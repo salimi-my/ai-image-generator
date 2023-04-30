efaeb0a0f4bfa6dc35664a02b99cfc35aec6ee30
Mohamad Salimi <contact@salimi.my>
2023-05-01 02:19:56 +0800

    Update cloudinary config

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -11,7 +11,8 @@ const router = express.Router();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
 });
 
 // get all posts
